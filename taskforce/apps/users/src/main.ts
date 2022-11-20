88b6c279eb95762cab1cf5f3fc5599f4cf0021ee
SergeyZasimov <zasimov.ser@yandex.ru>
2022-11-20 17:57:35 +0300

    Установлена и настроена библиотека для формирования документации.
    Установлена библиотека @nestjs/swagger.

--- taskforce/apps/users/src/main.ts
+++ taskforce/apps/users/src/main.ts
@@ -5,6 +5,8 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +14,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Taskforce')
+    .setDescription('The «Users» service API specification')
+    .setVersion('1.0')
+    .addTag('Users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
