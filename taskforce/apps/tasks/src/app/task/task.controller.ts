8f8eeef075d16d1bcf4a1abff5034634d735654e
SergeyZasimov <zasimov.ser@yandex.ru>
2022-11-23 06:38:59 +0300

    Добавлена документация для сервиса Tasks

--- taskforce/apps/tasks/src/app/task/task.controller.ts
+++ taskforce/apps/tasks/src/app/task/task.controller.ts
@@ -8,27 +8,39 @@ import {
   Patch,
   Query,
 } from '@nestjs/common/decorators';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { fillObject } from '@taskforce/core';
 import { CreateTaskDto } from '../dto/create-task.dto';
 import { UpdateTaskDto } from '../dto/update-task.dto';
-import { TaskRdo } from '../rdo/task.dto';
+import { TaskRdo } from '../rdo/task.rdo';
 import {
   TASKS_BY_CATEGORY_NOT_FOUND_ERROR,
   TASK_NOT_FOUND_ERROR,
 } from './task.const';
 import { TaskService } from './task.service';
 
+@ApiTags('task')
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post('')
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Create new task',
+    type: TaskRdo,
+  })
   public async create(@Body() dto: CreateTaskDto) {
     const newTask = await this.taskService.createTask(dto);
     return fillObject(TaskRdo, newTask);
   }
 
   @Get(':id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Information about a single task',
+    type: TaskRdo,
+  })
   public async show(@Param() { id }) {
     const existTask = await this.taskService.getTask(id);
 
@@ -40,6 +52,12 @@ export class TaskController {
   }
 
   @Get('')
+  @ApiQuery({name: 'Category'})
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Information about tasks of the same category',
+    type: [TaskRdo],
+  })
   public async getByCategory(@Query() { category }) {
     const tasks = await this.taskService.getByCategory(category);
     if (!tasks) {
@@ -49,6 +67,11 @@ export class TaskController {
   }
 
   @Patch(':id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Updating a task',
+    type: [TaskRdo],
+  })
   public async update(@Param() { id }, @Body() dto: UpdateTaskDto) {
     const existTask = this.taskService.getTask(id);
 
@@ -62,6 +85,10 @@ export class TaskController {
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: 'Deleting a task'
+  })
   public async delete(@Param() { id }) {
     const existTask = this.taskService.getTask(id);
 
--- taskforce/apps/tasks/src/main.ts
+++ taskforce/apps/tasks/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,6 +13,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Taskforce')
+    .setDescription('The «Tasks» service API specification')
+    .setVersion('1.0')
+    .addTag('Tasks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
