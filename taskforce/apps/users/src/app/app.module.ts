3158273ed4bfc79c7fb2ec6f75d3141e238247c7
SergeyZasimov <zasimov.ser@yandex.ru>
2022-11-23 12:03:14 +0300

    Добавлено пространство имен "database"

--- taskforce/apps/users/src/app/app.module.ts
+++ taskforce/apps/users/src/app/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_FILE_PATH } from './app.constant';
+import databaseConfig from './config/database.config';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig],
+    }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
