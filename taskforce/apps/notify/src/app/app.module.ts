f844af1dd23eef11d4835ac8c26a21c45f5356b4
SergeyZasimov <zasimov.ser@yandex.ru>
2022-12-23 10:47:18 +0300

    [notify] Добавлена настройка для подключения к mongodb

--- taskforce/apps/notify/src/app/app.module.ts
+++ taskforce/apps/notify/src/app/app.module.ts
@@ -5,15 +5,18 @@ import { ConfigModule } from '@nestjs/config';
 import { ENV_FILE_PATH } from './app.constant';
 import { getSmtpConfig, smtpOptions } from './config/smtp.config';
 import { rabbitMqOptions } from './config/rabbitmq.config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongodbConfig, mongodbOptions } from './config/mongodb.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ENV_FILE_PATH,
-      load: [smtpOptions, rabbitMqOptions],
+      load: [smtpOptions, rabbitMqOptions, mongodbOptions],
     }),
     MailerModule.forRootAsync(getSmtpConfig()),
+    MongooseModule.forRootAsync(getMongodbConfig()),
     EmailModule,
   ],
   controllers: [],
